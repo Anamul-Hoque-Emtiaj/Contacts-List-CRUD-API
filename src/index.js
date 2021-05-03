8322378c7b2bbbae80e74abbf49ccff92685f34e
A H Emtiaz <1905113@ugrad.cse.buet.ac.bd>
2021-05-03 12:28:35 +0600

    cng

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import axios from 'axios'
 const URL = "http://localhost:3000/contacts";
 
-window.onload = function(){
-    const Parent = document.getElementById('Parents')
-    axios.get(URL)
-    .then(res=>{
-        res.data.forEach(element => {
-            CreateRow(element,Parent);
-         });
-    })
-    .catch(err=>{
-    console.log(err);
-    })
-}
-
 const CreateRow = (contact,Parent) =>{
     let TR = document.createElement('tr')
     let tdName = document.createElement('td')
@@ -33,20 +20,57 @@ const CreateRow = (contact,Parent) =>{
     EditBtn.className = 'btn btn-warning'
     EditBtn.innerHTML = 'Edit'
     EditBtn.addEventListener('click',()=>{
-        
+        console.log('Edit Button Pressed')
     })
     tdActions.appendChild(EditBtn)
 
     let DeleteBtn = document.createElement('button')
     DeleteBtn.className = 'btn btn-danger'
     DeleteBtn.innerHTML = 'Delete'
-    DeleteBtn.addEventListener('click',()=>{
-
+    DeleteBtn.addEventListener('click',function(){
+        axios.delete('${URL}/${contact.id}')
+        .then(res =>{
+            Parent.removeChild(TR)
+        }).catch(err=>{
+            console.log('Error Occured')
+        })
+        
     })
     tdActions.appendChild(DeleteBtn)
     TR.appendChild(tdActions)
     Parent.appendChild(TR)
 }
 
+window.onload = function() {
+    const Parent = document.getElementById('Parents')
+    axios.get(URL)
+    .then(res=>{
+        
+        res.data.forEach(element => {
+            CreateRow(element,Parent);
+            console.log(res.data.name)
+         });
+    })
+    .catch(err=>{
+    console.log(err);
+    })
+    document.getElementById('submit').addEventListener('click',CreateNew)
+}
+
+const CreateNew = ()=>{
+    const contact = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        MobileNo: document.getElementById('pnum').value
+    }
+    axios.post(URL,contact)
+    .then(res=>{
+        const Parent = document.getElementById('Parents')
+        CreateRow(res.data,Parent)
+        console.log(res.data)
+    }).catch(err=>{
+        console.log("Error Occured")
+    })
+}
 
 
